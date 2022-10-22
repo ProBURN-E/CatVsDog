3b16b50930103e2c71e8c200b75131e5aec42b09
ProBURN-E <71887679+ProBURN-E@users.noreply.github.com>
2022-10-23 05:17:48 +0800

    Update streamlit_inference_server.py

--- streamlit_inference_server.py
+++ streamlit_inference_server.py
@@ -12,7 +12,7 @@ device = torch.device('cpu')
 model = MobileNetV2Model(num_classes=2).to(device)
 checkpoint = torch.hub.load_state_dict_from_url('https://public.ch.files.1drv.com/y4mapAGWfxALhSFloP0PDSg9HmM7PDe7SjiCZePZ6kYcCXW1rbMF50OUdFEXX_dUdTqdbBiaGOYWx_cgVK8jYzKkzX2fNurulS-LDJ2JlgH2nX0A7rW_p6WzlxKO5ThP9ewhH3z9ntU-FBmypiJEGyQK5hMR57q5XRgh2ogHmullFH5M129sVVmUXNm8qTo1SdGaaWjFYzMm1fCDssJReRDOATorTlTnnrPVI-6NTykWRo', map_location=device)
 state_dict = checkpoint['state_dict']
-model.load_state_dict(state_dict,map_location=device)
+model.load_state_dict(state_dict)
 
 # prepare model for testing
 model.eval()
